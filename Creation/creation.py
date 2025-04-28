94c22f6cfad6c95388e680cd783d01a7d4887a8a
AayushKotwani3 <aayushkotwani9@gmail.com>
2025-04-29 03:06:23 +0530

    Beautified and cleaned for better understanding

--- Creation/creation.py
+++ Creation/creation.py
@@ -1,38 +1,44 @@
 import numpy as np
 
-# One dimensional array
-arr=np.array([1,2,3,4])
-# print (arr)
+# ------------------------------------------
+# 1. Creating a One-Dimensional Array
+one_d_array = np.array([1, 2, 3, 4])
+# print(one_d_array)
 
-# two dimensional array
-arr2=np.array([[1,2,3,4],[5,6,7,8]])
-# print (arr2)
+# ------------------------------------------
+# 2. Creating a Two-Dimensional Array (Matrix)
+two_d_array = np.array([[1, 2, 3, 4],
+                        [5, 6, 7, 8]])
+# print(two_d_array)
 
-# with default as zero one dimensional here (3) is number of zeros in the row (shape)
+# ------------------------------------------
+# 3. Creating a One-Dimensional Array Filled with Zeros
+zero_array_1d = np.zeros(3)   # 3 elements, all zeros
+# print(zero_array_1d)
 
-arrz=np.zeros(3)
-# print (arrz)
+# ------------------------------------------
+# 4. Creating a Two-Dimensional Array Filled with Zeros
+zero_array_2d = np.zeros((3, 4))   # Shape = 3 rows × 4 columns
+# print(zero_array_2d)
 
-#with two dimensional zeroes here (3) is number of rows and (4) is number of columns
-arrz2=np.zeros((3,4))  #(3,4) is called as shape 
-# print (arrz2)
+# ------------------------------------------
+# 5. Creating a Two-Dimensional Array Filled with Ones
+ones_array = np.ones((2, 3))    # Shape = 2 rows × 3 columns
+# print(ones_array)
 
-# if we want the defailt values to be ones
+# ------------------------------------------
+# 6. Creating a Two-Dimensional Array Filled with a Specific Value
+filled_array = np.full((2, 3), 5)   # Shape = 2x3, filled with 5
+# print(filled_array)
 
-arrones=np.ones((2,3))
-# print(arrones)
+# ------------------------------------------
+# 7. Creating a Sequence of Numbers
+# Using arange(start, stop, step)
+sequence_array = np.arange(1, 11, 2)   # From 1 to 10 (exclusive 11), step by 2
+# print(sequence_array)
 
-# if we want default value not zero or one we can use full function
-arrfull=np.full((2,3),5) #(2,3) is shape and 5 is the default value we want
-# print(arrfull)
-
-# creating sequences of numbers in numpy
-#using arange() function
-#arange(start,stop,step) start is the starting value,stop is the last value it will be excluded and run upto -1 and step is number by which we need to step up or say skip
-arrarange=np.arange(1,11,2)
-# print(arrarange)
-
-# crating Identity matrix using numpy
-#we will use eye() function eye(size)
-Identity_matrix=np.eye(5)
-print(Identity_matrix)
+# ------------------------------------------
+# 8. Creating an Identity Matrix
+# Identity matrix = square matrix with 1s on the diagonal and 0s elsewhere
+identity_matrix = np.eye(5)   # 5x5 Identity matrix
+print(identity_matrix)
